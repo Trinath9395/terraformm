febd98f7d743a434d62719469b76bbf1c81acb2d
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-06 10:41:52 +0530

    locals

---
+++ locals/variable.tf
@@ -0,0 +1,17 @@
+variable "aws_vpc" {
+  type = string
+  default = "vpc-0cff62a4c9dcdf272"
+}
+
+variable "project" {
+  default = "expense"
+}
+
+variable "component" {
+  default = "backend"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
